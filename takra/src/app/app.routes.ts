6c9d72d31dee13ad92d3583b28e34c95dcd5b9b6
PinkMercy <rahmabacha007@gmail.com>
2025-03-26 13:42:22 +0100

    loginsigup

--- takra/src/app/app.routes.ts
+++ takra/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { CalendarComponent } from './pages/calendar/calendar.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 export const routes: Routes = [
   { path: '', component: SidebarComponent, children: [
@@ -9,6 +10,7 @@ export const routes: Routes = [
     { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) }
   ]},
   { path: 'login', component: LoginComponent }, 
+  {path: 'signup', component: SignupComponent},
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) }
 ];
